ad590457231c02f13a01dae07b2363a473ae1466
Alex Astafev <efsneiron@gmail.com>
2022-08-21 19:47:31 +0300

    added jwt auth

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,10 @@
 from django.urls import include, path
 from rest_framework import routers
 from .views import UserViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 router = routers.DefaultRouter()
 router.register(r"users", UserViewSet)
@@ -8,4 +12,6 @@ router.register(r"users", UserViewSet)
 urlpatterns = [
     path("", include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
