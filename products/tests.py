21d8cc835e07e2370f6501b38e0b6df428964dd4
Alex Astafev <efsneiron@gmail.com>
2022-09-04 20:04:54 +0300

    corrected test

--- products/tests.py
+++ products/tests.py
@@ -12,17 +12,26 @@ class TestProductListViewSet(APITestCase):
         response = self.client.get("/api/v1/products/")
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertTrue("results" in response.data.keys())
+
+        paginated_keys = response.data.keys()
+
+        self.assertIn("links", paginated_keys)
+        self.assertIn("total", paginated_keys)
+        self.assertIn("pages", paginated_keys)
+        self.assertIn("page", paginated_keys)
+        self.assertIn("results", paginated_keys)
 
         product = response.data["results"][0]
+        product_keys = product.keys()
 
-        self.assertIn("id", product.keys())
-        self.assertIn("name", product.keys())
-        self.assertIn("description", product.keys())
-        self.assertIn("price", product.keys())
-        self.assertIn("media", product.keys())
+        self.assertIn("id", product_keys)
+        self.assertIn("name", product_keys)
+        self.assertIn("description", product_keys)
+        self.assertIn("price", product_keys)
+        self.assertIn("media", product_keys)
 
         media_item = product["media"][0]
+        media_item_keys = media_item.keys()
 
-        self.assertIn("id", media_item.keys())
-        self.assertIn("path", media_item.keys())
+        self.assertIn("id", media_item_keys)
+        self.assertIn("path", media_item_keys)
