011c4fee47a34e7640a570814b64959589cab51a
Alex Astafev <efsneiron@gmail.com>
2022-09-11 14:31:10 +0300

    added page_size

--- config/pagination.py
+++ config/pagination.py
@@ -1,9 +1,12 @@
+import math
+
 from rest_framework import pagination
 from rest_framework.response import Response
-import math
 
 
 class CustomPagination(pagination.PageNumberPagination):
+    page_size_query_param = "page_size"
+
     def get_paginated_response(self, data):
         count = self.page.paginator.count
 
@@ -11,7 +14,7 @@ class CustomPagination(pagination.PageNumberPagination):
             {
                 "links": {"next": self.get_next_link(), "previous": self.get_previous_link()},
                 "total": count,
-                "pages": math.ceil(count / self.page_size),
+                "pages": math.ceil(count / self.get_page_size(self.request)),
                 "page": self.page.number,
                 "results": data,
             }
