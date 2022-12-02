be2daeabe73589eae9449afc1f4168e492f31317
Alex Astafev <efsneiron@gmail.com>
2022-12-02 20:43:05 +0300

    added a template for email

--- orders/services.py
+++ orders/services.py
@@ -1,10 +1,15 @@
-from django.core.mail import EmailMessage
+from django.core.mail import send_mail, EmailMultiAlternatives
+from django.template.loader import render_to_string
 from .models import Order
 
 
 def send_order_to_email(order: Order, emails: list[str]) -> None:
     """Send an order to the list of emails with template"""
-    message = EmailMessage(
-        "У вас новый заказ", "<strong>test</strong>", "no-replay@aeroglissers.ru", emails
+
+    body = render_to_string("orders/email/order.html", )
+    message = EmailMultiAlternatives(
+        subject="Новый заказ на " + order.product.name,
+        to=emails
     )
+    message.attach_alternative(body, "text/html")
     message.send(fail_silently=False)
