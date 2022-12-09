e0b70381f30de90363fc3d228da2d6b5fa4962a2
Alex Astafev <efsneiron@gmail.com>
2022-12-09 09:51:32 +0300

    added a bit translations

--- config/validators.py
+++ config/validators.py
@@ -1,19 +1,22 @@
 from django.core.exceptions import ValidationError
 import re
+from django.utils.translation import gettext as _
 
 
 def file_size(file):
     limit = 2
 
     if file.size > limit * 1024 * 1024:
-        raise ValidationError(f"File size must be lower than {limit}")
+        raise ValidationError(_("File size must be lower than %(limit)d.") % {'limit': limit})
 
 
 def full_name(value: str):
+    message = _("Incorrect full name %(value)s.") % {'value': value}
+
     if not re.match("^[a-zA-zА-Яа-я ']+$", value):
-        raise ValidationError(f"Incorrect full name {value}")
+        raise ValidationError(message)
 
     value = re.sub(" +", " ", value).strip()
 
     if len(value.split(" ")) < 2:
-        raise ValidationError(f"Incorrect full name {value}")
+        raise ValidationError(message)
